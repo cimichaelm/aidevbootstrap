c9c1514ca1222064ba7b676c9a4796ce8a3141cf
cimichaelm <michaelm@corp.citenet.net>
2023-12-11 12:37:32 -0500

    update

--- scripts/setup-ai-env.sh
+++ scripts/setup-ai-env.sh
@@ -86,10 +86,22 @@ setup_work()
     
 }
 
+setup_miniconda()
+{
+    mkdir -p ~/miniconda3
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+    bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+    rm -rf ~/miniconda3/miniconda.sh
+    ~/miniconda3/bin/conda init bash
+    ~/miniconda3/bin/conda init zsh
+}
+
 defaults
 
 setup_pkgs
 setup_system
+
+setup_miniconda
 
 setup_python_venv
 setup_python $requirements
